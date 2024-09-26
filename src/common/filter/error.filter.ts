0a945624a178c7d0008db43386e95e02a5811cb1
vergaraCoder <jhonatanvergara072@gmail.com>
2024-09-25 21:53:54 -0500

    changes

--- src/common/filter/error.filter.ts
+++ src/common/filter/error.filter.ts
@@ -13,18 +13,17 @@ export class HttpFilter implements ExceptionFilter{
         let message:any= "";
         let status:number= 0;
 
-        switch(exception.message.split(" :: ")){
-            case ifExist:
-                message=ifExist;
-                status=exception.message.split(" :: ")[0];
-                break;
-            case null:
-                message="Bad Request";
-                status= 400;
-                break;
-            default:
-                message=HttpStatus.INTERNAL_SERVER_ERROR;
-                status=500;
+        if(exception.response.message){
+            message="the properties are bad";
+            status=400;
+        }
+        else if(ifExist){
+            message=ifExist;
+            status=parseInt(exception.message.split(" :: ")[0]);
+        }
+        else{
+            message="INTERNAL SERVER ERROR";
+            status=500;
         }
 
         response.status(status).json({
